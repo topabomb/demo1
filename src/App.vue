282929d9a41430edf4f163e67e0deb9afb752a82
Topabomb <hualei.hb@gmail.com>
2026-08-21 00:38:13 +0800

    feat: make architecture the entry point and lab an explicit route

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup lang="ts">
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
 import AgentWorkspace from './components/AgentWorkspace.vue'
+import ArchitectureOverview from './components/ArchitectureOverview.vue'
+
+const hash = ref(window.location.hash)
+const view = computed(() => hash.value === '#lab' ? 'lab' : 'architecture')
+
+function syncHash(): void { hash.value = window.location.hash }
+onMounted(() => window.addEventListener('hashchange', syncHash))
+onBeforeUnmount(() => window.removeEventListener('hashchange', syncHash))
 </script>
 
 <template>
-  <AgentWorkspace />
+  <AgentWorkspace v-if="view === 'lab'" />
+  <ArchitectureOverview v-else />
 </template>
