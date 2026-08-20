e95f7f3d13b106fddd32ae9eb7acca4c28c65e21
Topabomb <hualei.hb@gmail.com>
2026-08-20 23:41:00 +0800

    refactor: subscribe each rendered seat to one stable node

---
+++ src/components/ConversationNodeSeat.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { onBeforeUnmount, shallowRef, watch } from 'vue'
+import type { ConversationSessionRuntime } from '../conversation/session-runtime'
+import RenderUnitView from './RenderUnitView.vue'
+
+const props = defineProps<{ runtime: ConversationSessionRuntime; nodeId: string }>()
+const unit = shallowRef(props.runtime.projection.getNode(props.nodeId))
+let unsubscribe: (() => void) | null = null
+
+function bind(runtime: ConversationSessionRuntime, nodeId: string): void {
+  unsubscribe?.()
+  unit.value = runtime.projection.getNode(nodeId)
+  unsubscribe = runtime.projection.subscribeNode(nodeId, () => {
+    unit.value = runtime.projection.getNode(nodeId)
+  })
+}
+
+watch(() => [props.runtime, props.nodeId] as const, ([runtime, nodeId]) => bind(runtime, nodeId), { immediate: true })
+onBeforeUnmount(() => unsubscribe?.())
+</script>
+
+<template>
+  <RenderUnitView v-if="unit" :unit="unit" />
+</template>
