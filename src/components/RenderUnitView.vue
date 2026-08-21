60d7044af90e9bbee9e2b15d4d8c5752815f3002
Topabomb <hualei.hb@gmail.com>
2026-08-21 22:34:36 +0800

    refactor: import RenderUnit from presentation

--- src/components/RenderUnitView.vue
+++ src/components/RenderUnitView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { RenderUnit } from '../core/types'
+import type { RenderUnit } from '../presentation/render-unit'
 import { resolveRenderer } from './renderers/registry'
 
 const props = defineProps<{ unit: RenderUnit }>()
