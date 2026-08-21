b240082ddf9a544723282923d99f00f2aa2b934b
Topabomb <hualei.hb@gmail.com>
2026-08-21 23:37:51 +0800

    refactor: make demo entry explicitly compose engine and demo styles

--- src/demo/main.ts
+++ src/demo/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/demo.css'
-import './styles/engine.css'
-import './architecture.css'
+import '../engine/vue/engine.css'
+import './styles/architecture.css'
 
 createApp(App).mount('#app')
