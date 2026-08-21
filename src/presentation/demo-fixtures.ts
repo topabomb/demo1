478a658cd0293e2b4a83262b0d07a81ee3184999
Topabomb <hualei.hb@gmail.com>
2026-08-21 22:28:22 +0800

    refactor: remove demo scenario compatibility shim

--- src/presentation/demo-fixtures.ts
+++
@@ -1,9 +0,0 @@
-/** @deprecated Demo compatibility path. Import from `demo/scenarios`. */
-export {
-  AGENT_SCENARIO_MESSAGE_COUNT,
-  MARKDOWN_COMPATIBILITY_FIXTURES,
-  createAgentScenarioPack,
-  createMarkdownGalleryTurn,
-  createMixedDemoTurn,
-  createMixedDemoTurns,
-} from '../demo/scenarios'
