11733e00d0cded6808a863a63f593c15d44bab2d
Topabomb <hualei.hb@gmail.com>
2026-08-21 22:36:04 +0800

    refactor: import render estimate contract explicitly

--- src/core/estimates.ts
+++ src/core/estimates.ts
@@ -1,4 +1,4 @@
-import type { RenderUnit } from './types'
+import type { RenderUnit } from '../presentation/render-unit'
 
 export function estimateUnitHeight(unit: RenderUnit | undefined): number {
   return unit?.estimatePx ?? 180
