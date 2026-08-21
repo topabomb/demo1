cae4d70c892cae2f6f34e777e188dc4940fb6f75
Topabomb <hualei.hb@gmail.com>
2026-08-21 11:21:04 +0800

    refactor: resolve renderers through registry

--- src/components/RenderUnitView.vue
+++ src/components/RenderUnitView.vue
@@ -1,29 +1,12 @@
 <script setup lang="ts">
-import type { Component } from 'vue'
-import type { RenderKind, RenderUnit } from '../core/types'
-import TextBlock from './renderers/TextBlock.vue'
-import MarkdownBlock from './renderers/MarkdownBlock.vue'
-import ThinkingBlock from './renderers/ThinkingBlock.vue'
-import CodeBlock from './renderers/CodeBlock.vue'
-import ImageBlock from './renderers/ImageBlock.vue'
-import HtmlBlock from './renderers/HtmlBlock.vue'
-import ToolBlock from './renderers/ToolBlock.vue'
-import DiffBlock from './renderers/DiffBlock.vue'
+import { computed } from 'vue'
+import type { RenderUnit } from '../core/types'
+import { resolveRenderer } from './renderers/registry'
 
-defineProps<{ unit: RenderUnit }>()
-
-const registry: Record<RenderKind, Component> = {
-  text: TextBlock,
-  markdown: MarkdownBlock,
-  thinking: ThinkingBlock,
-  code: CodeBlock,
-  image: ImageBlock,
-  html: HtmlBlock,
-  tool: ToolBlock,
-  diff: DiffBlock,
-}
+const props = defineProps<{ unit: RenderUnit }>()
+const component = computed(() => resolveRenderer(props.unit.kind))
 </script>
 
 <template>
-  <component :is="registry[unit.kind]" :unit="unit" />
+  <component :is="component" :unit="unit" />
 </template>
