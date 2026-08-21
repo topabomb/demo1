7b1c4f1774cc1274e06f5c415e4468d93b44a0ed
Topabomb <hualei.hb@gmail.com>
2026-08-21 22:35:38 +0800

    refactor: import markdown renderer contract directly

--- src/components/renderers/MarkdownBlock.vue
+++ src/components/renderers/MarkdownBlock.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import type { RenderUnit } from '../../core/types'
+import type { RenderUnit } from '../../presentation/render-unit'
 import { renderMarkdownCached } from './markdown-cache'
 
 const props = defineProps<{ unit: RenderUnit }>()
