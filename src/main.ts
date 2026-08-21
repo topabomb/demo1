8b95f6ac8183d6d50287cfa91b81d6ce5d488b75
Topabomb <hualei.hb@gmail.com>
2026-08-21 11:23:13 +0800

    style: load renderer containment layer

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import './styles.css'
 import './agent-renderers.css'
 import './virtua-layout.css'
+import './renderer-content.css'
 import './architecture.css'
 import './product-ux.css'
 
